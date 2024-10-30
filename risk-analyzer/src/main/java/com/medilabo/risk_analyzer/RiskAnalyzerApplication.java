9577ab7791a1e8ba389a381d051aa4ec03f142c3
Psolyvem <psolyvem@gmail.com>
2024-10-30 02:35:50 +0100

    Added risk analyzer module

---
+++ risk-analyzer/src/main/java/com/medilabo/risk_analyzer/RiskAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.risk_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskAnalyzerApplication.class, args);
+	}
+
+}
---
+++ risk-analyzer/src/test/java/com/medilabo/risk_analyzer/RiskAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.risk_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
